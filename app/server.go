1f1b330e6f3cfa440798112a8b2625747152f8be
nidhey27 <nidhey.indurkar@infracloud.io>
2023-12-27 16:32:16 +0530

    pass 3rd stage

--- app/server.go
+++ app/server.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -21,11 +22,49 @@ func main() {
 		os.Exit(1)
 	}
 
-	response := "HTTP/1.1 200 OK\r\n\r\n"
+	buffer := make([]byte, 8192)
+	length, err := conn.Read(buffer)
+
+	if err != nil {
+		fmt.Println("Error reading: ", err.Error())
+		os.Exit(1)
+		os.Exit(1)
+	}
+
+	request := string(buffer[:length])
+	fmt.Println("Received request:")
+	fmt.Println(request)
+	r := parseRequest(request)
+	var response string
+	if r.Path == "/" {
+		response = "HTTP/1.1 200 OK\r\n\r\n"
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
 	_, err = conn.Write([]byte(response))
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 	}
 
 	defer conn.Close()
+}
+
+type Request struct {
+	Method  string
+	Path    string
+	Headers map[string]string
+}
+
+func parseRequest(request string) Request {
+	lines := strings.Split(request, "\r\n")
+	startLine := lines[0]
+	components := strings.Split(startLine, " ")
+	method := components[0]
+	path := components[1]
+	return Request{
+		Method:  method,
+		Path:    path,
+		Headers: make(map[string]string),
+	}
 }
